c65296ae08c73aa45c1687b5d74123a3d53881cd
yanakiv30 <yanakiv30@gmail.com>
2023-12-15 22:55:02 +0200

    items-item

--- src/App.js
+++ src/App.js
@@ -21,16 +21,30 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(5);
+  const [quantity, setQuantity] = useState(1);
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   function handleSubmit(e) {
-    e.preventDefault()
-    console.log(e);
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+    
+
+    handleAddItems(newItem);
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+
   };
 
 
   return <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your 😎 trip</h3>
-    <select value={quantity} onChange={(e=> setQuantity(+e.target.value))}>
+    <select value={quantity} onChange={(e => setQuantity(+e.target.value))}>
       {Array.from({ length: 20 }, (_, i) => i + 1)
         .map(num => (<option value={num} key={num}>{num}</option>))}
       {/* <option value={2}>2</option>  not dynamic */}
@@ -38,7 +52,7 @@ function Form() {
     <input type="text" placeholder="Item..."
       value={description}
       onChange={e => setDescription(e.target.value)}
-       />
+    />
     <button>ADD</button>
 
   </form>
