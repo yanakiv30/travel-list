844cb3942478dd8913ca79c84753f96672a3bd80
yanakiv30 <yanakiv30@gmail.com>
2023-12-19 10:55:51 +0200

    lift-up-state-1

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return <div className="app">
     <Logo />
-    <Form />
-    <PackingList />
+    <Form onAddItems={handleAddItems}/>
+    <PackingList items={items} />
     <Stats />
   </div>
 }
@@ -19,23 +24,21 @@ function Logo() {
   return <h1>🌴 Far Away 👜</h1>
 }
 
-function Form() {
+function Form({onAddItems}) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-  const [items, setItems] = useState([]);
+  
 
-  function handleAddItems(item) {
-    setItems((items) => [...items, item]);
-  }
+ 
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() }
     
-
-    handleAddItems(newItem);
     console.log(newItem);
+    onAddItems(newItem);
+    
     setDescription("");
     setQuantity(1);
 
@@ -58,11 +61,11 @@ function Form() {
   </form>
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul >
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
 
         ))}
